2262ffc78682a5e032f885edd3e5a9f53bd47de1
Juli Azzini <jazzini@teco.com.ar>
2021-11-01 17:12:26 -0300

    debug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,34 +21,4 @@ export default NextAuth({
       clientSecret: process.env.NEXT_PUBLIC_IDP_CLIENT_SECRET,
     },
   ],
-  debug: true,
-  session: {
-    jwt: true,
-    maxAge: 60 * 5,
-  },
-  jwt: {
-    encryption: false,
-  },
-  callbacks: {
-    async signin(user, account, profile) {
-      console.log("user", user, account, profile);
-      return true;
-    },
-    async jwt(token, user, account, profile, isNewUser) {
-      console.log(token);
-      console.log(user);
-      console.log(account);
-      console.log(profile);
-      console.log(isNewUser);
-      if (account.accessToken) {
-        token.accessToken = account.accessToken;
-      }
-      return Promise.resolve(token);
-    },
-    async session(session, token) {
-      console.log(session);
-      console.log(token);
-      return session;
-    },
-  },
 });
