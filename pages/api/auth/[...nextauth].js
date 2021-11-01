3026c790aaa04b1b79839599ceccf11b4e235117
Juli Azzini <jazzini@teco.com.ar>
2021-11-01 17:56:04 -0300

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,25 +6,14 @@ export default NextAuth({
       id: "idp",
       name: "Portal Unificado",
       type: "oauth",
-      version: "2.0",
-      scope: "openid email",
-      state: true,
-      protection: "state",
-      params: { grant_type: "authorization_code" },
-      idToken: true,
-      authorizationUrl:
-        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/authorize?response_type=code",
-      accessTokenUrl:
-        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
-      requestTokenUrl:
+      authorization:
         "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/authorize",
-      profileUrl:
+      token:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
+      userinfo:
         "https://idpsesiont.telecom.com.ar:443/openam/oauth2/convergente/userinfo",
       async profile(profile) {
         console.log(profile);
-        return {
-          id: profile.id,
-        };
       },
       clientId: process.env.NEXT_PUBLIC_IDP_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_IDP_CLIENT_SECRET,
