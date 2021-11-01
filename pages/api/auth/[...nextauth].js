d6051ca3a9d85101d92ef277af462c29d59408d1
Juli Azzini <jazzini@teco.com.ar>
2021-11-01 16:33:09 -0300

    changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,23 +3,15 @@ import NextAuth from "next-auth/next";
 export default NextAuth({
   providers: [
     {
-      id: "idp",
-      name: "Portal Unificado",
+      id: "kakao",
+      name: "Kakao",
       type: "oauth",
-      version: "2.0",
-      scope: "openid email",
-      state: true,
-      protection: "state",
-      params: { grant_type: "authorization_code" },
-      idToken: true,
-      authorizationUrl:
-        "https://idpsesion.telecom.com.ar/openam/oauth2/realms/convergente/authorize?response_type=code",
-      accessTokenUrl:
-        "https://idpsesion.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
-      requestTokenUrl:
-        "https://idpsesion.telecom.com.ar/openam/oauth2/realms/convergente/connect/jwk_uri",
-      profileUrl:
-        "https://idpsesion.telecom.com.ar:443/openam/oauth2/convergente/userinfo",
+      authorization:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/authorize",
+      token:
+        "https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token",
+      userinfo:
+        "https://idpsesiont.telecom.com.ar:443/openam/oauth2/convergente/userinfo",
       profile(profile) {
         return {
           id: profile.id,
