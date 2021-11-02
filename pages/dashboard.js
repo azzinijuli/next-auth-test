4eeef32c6562711e05c9fa4016554ee8b37d4864
Juli Azzini <jazzini@teco.com.ar>
2021-11-02 12:57:26 -0300

    fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,13 @@
+import { useSession } from "next-auth/react";
+
 export default function Dashboard() {
-  return <span>Redirection success</span>;
+  const { data: session } = useSession();
+  console.log("acá", session);
+  return (
+    <>
+      <span>Redirection success</span>;
+      {session ?? <p>Signed in as {session}</p>}
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  );
 }
