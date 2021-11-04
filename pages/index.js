9457799b44bafe49b61519e850397878b8111f1e
Juli Azzini <jazzini@teco.com.ar>
2021-11-04 16:53:35 -0300

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import { useEffect } from "react";
 
 export default function Component() {
   const { data: session } = useSession();
-  // const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-  // const externalId = session.user.name;
+  const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+  const externalId = session.user.name;
 
   useEffect(() => {
     fetchData();
   }, []);
 
   async function fetchData() {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+    const res = await fetch(
+      `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
+    );
     const data = await res.json();
     console.log(data);
   }
