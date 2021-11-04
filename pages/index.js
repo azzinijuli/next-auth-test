b74f8d6772262b081f9689868485b6387d0aace9
Juli Azzini <jazzini@teco.com.ar>
2021-11-04 12:54:48 -0300

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Component() {
   const { data: session } = useSession();
-  console.log(session);
+  const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+  const externalId = session.user.name;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const res = await fetch(
+      `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
+    );
+    const data = await res.json();
+    console.log(data);
+  }
+
   if (session) {
     return (
       <>
