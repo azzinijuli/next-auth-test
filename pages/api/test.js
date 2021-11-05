1a1254c19dbfec2c223ec7229ac65d3ba41a6955
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 12:20:17 -0300

    try api back

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function handler(req, res) {
+  const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+  const externalId = 543777220160;
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const data = await fetch(
+    `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
+  );
+  const json = await data.json();
+  console.log("json", json);
+  console.log("req", req);
+  res.status(200).json(json);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useEffect } from "react";
 
 export default function Component() {
   const { data: session } = useSession();
-  const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-  const externalId = session.user.name;
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  async function fetchData() {
-    const res = await fetch(
-      `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
-    );
-    const data = await res.json();
-    console.log(data);
-  }
 
   if (session) {
     return (
