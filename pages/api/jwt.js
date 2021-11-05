c78b221d5cedba36922bdfea8c4973231e96b7b8
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 17:13:33 -0300

    changes

--- pages/api/jwt.js
+++ pages/api/jwt.js
@@ -1,35 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// export default async function handler(req, res) {
-// const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-// const externalId = 543777220160;
-
-// const data = await fetch(
-//   `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
-// );
-// const json = await data.json();
-// console.log("json", json);
-// res.status(200).json(json);
-// }
-
-// export default async function handler(req, res) {
-//   const code = req.query.code;
-//   const newUrl = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token?code=${code}&grant_type=authorization_code&redirect_uri=https://idp-nextjs-test.netlify.app/api/auth/callback/idp`;
-//   const encoded = Buffer.from(
-//     `${process.env.NEXT_PUBLIC_IDP_CLIENT_ID}:${process.env.NEXT_PUBLIC_IDP_CLIENT_SECRET}`,
-//     "binary"
-//   ).toString("base64");
-
-//   const response = await fetch(newUrl, {
-//     method: "post",
-//     headers: {
-//       "Content-Type": "application/x-www-form-urlencoded",
-//       Authorization: `Basic ${encoded}`,
-//     },
-//   });
-//   const data = await response.json();
-//   res.redirect(`/dashboard?token=${data.id_token}`);
-// }
-
 import { getToken } from "next-auth/jwt";
 
 const secret = process.env.NEXT_PUBLIC_IDP_SECRET;
--- pages/api/session.js
+++ pages/api/session.js
@@ -4,10 +4,51 @@ export default async function handler(req, res) {
   const session = await getSession({ req });
   if (session) {
     // Signed in
-    console.log("Session", JSON.stringify(session, null, 2));
+    const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+    const externalId = session.user?.name;
+    const response = await fetch(
+      `${baseUrlStore}/v1/customers/${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   } else {
     // Not Signed in
     res.status(401);
   }
   res.end();
 }
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// export default async function handler(req, res) {
+// const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+// const externalId = 543777220160;
+
+// const data = await fetch(
+//   `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
+// );
+// const json = await data.json();
+// console.log("json", json);
+// res.status(200).json(json);
+// }
+
+// export default async function handler(req, res) {
+//   const code = req.query.code;
+//   const newUrl = `https://idpsesiont.telecom.com.ar/openam/oauth2/realms/convergente/access_token?code=${code}&grant_type=authorization_code&redirect_uri=https://idp-nextjs-test.netlify.app/api/auth/callback/idp`;
+//   const encoded = Buffer.from(
+//     `${process.env.NEXT_PUBLIC_IDP_CLIENT_ID}:${process.env.NEXT_PUBLIC_IDP_CLIENT_SECRET}`,
+//     "binary"
+//   ).toString("base64");
+
+//   const response = await fetch(newUrl, {
+//     method: "post",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//       Authorization: `Basic ${encoded}`,
+//     },
+//   });
+//   const data = await response.json();
+//   res.redirect(`/dashboard?token=${data.id_token}`);
+// }
