710d3647cd707f70b6ce37fe82d03a289b4a91fc
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 17:18:50 -0300

    change res

--- pages/api/session.js
+++ pages/api/session.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       }
     );
     const data = await response.json();
-    console.log(data);
+    res.status(200).json(data);
   } else {
     // Not Signed in
     res.status(401);
