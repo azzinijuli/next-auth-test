6261b69ccbd04551fb147005d95cdc0d394441d0
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 14:42:19 -0300

    debug

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,16 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default async function handler(req, res) {
-  // const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-  // const externalId = 543777220160;
+// export default async function handler(req, res) {
+// const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+// const externalId = 543777220160;
 
-  // const data = await fetch(
-  //   `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
-  // );
-  // const json = await data.json();
-  // console.log("json", json);
-  console.log("req", req);
-  // res.status(200).json(json);
-}
+// const data = await fetch(
+//   `${baseUrlStore}}/v1/customers/${externalId}/products?status=PURCHASED,CANCELLED,EXPIRED`
+// );
+// const json = await data.json();
+// console.log("json", json);
+// res.status(200).json(json);
+// }
 
 // export default async function handler(req, res) {
 //   const code = req.query.code;
@@ -30,3 +29,30 @@ export default async function handler(req, res) {
 //   const data = await response.json();
 //   res.redirect(`/dashboard?token=${data.id_token}`);
 // }
+
+import { useSession, getSession } from "next-auth/react";
+
+export default function Page() {
+  console.log(session);
+  const { data: session } = useSession();
+
+  if (typeof window !== "undefined") return null;
+
+  if (session) {
+    return (
+      <>
+        <h1>Protected Page</h1>
+        <p>You can view this page because you are signed in.</p>
+      </>
+    );
+  }
+  return <p>Access Denied</p>;
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
+}
