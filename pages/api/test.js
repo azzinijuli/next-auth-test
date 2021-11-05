48d63c0ab678d770c407b7de1d2ac6e6cb448aa2
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 15:51:28 -0300

    fix

--- pages/api/test.js
+++ pages/api/test.js
@@ -35,6 +35,6 @@ import jwt from "next-auth/jwt";
 const secret = process.env.NEXT_PUBLIC_IDP_SECRET;
 
 export default async function handler(req, res) {
-  const token = await jwt.getToken({ req, secret });
+  const token = await jwt.getToken(req, secret);
   res.send(JSON.stringify(token, null, 2));
 }
