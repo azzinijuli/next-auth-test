61b1ebb44176cd1a0b4853a49f6bf1b9bd274bf1
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 17:21:25 -0300

    fix

--- pages/api/session.js
+++ pages/api/session.js
@@ -18,7 +18,6 @@ export default async function handler(req, res) {
     // Not Signed in
     res.status(401);
   }
-  res.end();
 }
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
