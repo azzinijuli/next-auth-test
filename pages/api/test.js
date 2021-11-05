2ea4298793a656a4d27b79223695eb445069c119
Juli Azzini <jazzini@teco.com.ar>
2021-11-05 15:23:42 -0300

    fix

--- pages/api/test.js
+++ pages/api/test.js
@@ -30,29 +30,9 @@
 //   res.redirect(`/dashboard?token=${data.id_token}`);
 // }
 
-import { useSession, getSession } from "next-auth/react";
+import { getSession } from "next-auth/client";
 
-export default function Page() {
-  const { data: session } = useSession();
-  console.log(session);
-
-  if (typeof window !== "undefined") return null;
-
-  if (session) {
-    return (
-      <>
-        <h1>Protected Page</h1>
-        <p>You can view this page because you are signed in.</p>
-      </>
-    );
-  }
-  return <p>Access Denied</p>;
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      session: await getSession(context),
-    },
-  };
+export default async function handler(req, res) {
+  const session = await getSession(req);
+  res.send(JSON.stringify(session, null, 2));
 }
