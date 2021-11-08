fde82ae3c7fa963e6382bdbb0b9127bd039d5fbc
Juli Azzini <jazzini@teco.com.ar>
2021-11-08 12:35:08 -0300

    get session

--- pages/api/session.js
+++ pages/api/session.js
@@ -2,28 +2,29 @@ import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
-  const token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
-  if (session) {
-    // Signed in
-    const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-    const externalId = session.user?.name;
-    const response = await fetch(
-      `${baseUrlStore}/v1/customers/${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    const data = await response.json();
-    res.status(200).json(data);
-  } else {
-    // Not Signed in
-    res.status(401);
-  }
+  console.log(session);
+  // const token =
+  //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
+  // if (session) {
+  //   // Signed in
+  //   const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+  //   const externalId = session.user?.name;
+  //   const response = await fetch(
+  //     `${baseUrlStore}/v1/customers/${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   res.status(200).json(data);
+  // } else {
+  //   // Not Signed in
+  //   res.status(401);
+  // }
 }
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
