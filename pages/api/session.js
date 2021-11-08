e4f4134a1d6fbe65596ade310af62ae6234d5ae3
Juli Azzini <jazzini@teco.com.ar>
2021-11-08 16:53:12 -0300

    test

--- pages/api/session.js
+++ pages/api/session.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const token =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
   // Signed in
-  const externalId = 10007043;
+  const externalId = 2932448128;
   const newUrl = `https://backoffice-staging.personal-svcs.com/v1/customers/54${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`;
   const response = await fetch(newUrl, {
     method: "GET",
