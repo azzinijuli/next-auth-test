4dd5aed6ebb61e639899e0b6dffde498b17c36e6
Juli Azzini <jazzini@teco.com.ar>
2021-11-08 15:49:52 -0300

    debug

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,29 +1,24 @@
 import { getToken } from "next-auth/jwt";
-import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
   const secret = process.env.NEXT_PUBLIC_IDP_SECRET;
   const stringifiedToken = await getToken({ req, secret });
-  const session = await getSession({ req });
   const token =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
-  if (session) {
-    // Signed in
-    const baseUrlStore = "https://backoffice-staging.personal-svcs.com/"; //UAT
-    const externalId = stringifiedToken.sub;
-    const newUrl = `${baseUrlStore}/v1/customers/54${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`;
-    const response = await fetch(newUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    console.log(stringifiedToken.sub);
-  } else {
-    // Not Signed in
-    res.status(401);
-  }
+  // Signed in
+  const baseUrlStore = "https://backoffice-staging.personal-svcs.com/"; //UAT
+  const externalId = stringifiedToken.sub;
+  const newUrl = `${baseUrlStore}/v1/customers/54${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`;
+  const response = await fetch(newUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  console.log(stringifiedToken.sub);
+  // Not Signed in
+  res.status(200).json(data);
 }
