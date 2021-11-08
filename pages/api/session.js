bf3d0aa36799cfdc38e53ab4268646c64bc81715
Juli Azzini <jazzini@teco.com.ar>
2021-11-08 15:19:59 -0300

    try user

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,29 +1,30 @@
 import { getToken } from "next-auth/jwt";
+import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
   const secret = process.env.NEXT_PUBLIC_IDP_SECRET;
-  const token = await getToken({ req, secret });
-  console.log("JSON Web Token", token.sub);
-  // const token =
-  //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
-  // if (session) {
-  //   // Signed in
-  //   const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
-  //   const externalId = session.user?.name;
-  //   const response = await fetch(
-  //     `${baseUrlStore}/v1/customers/${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     }
-  //   );
-  //   const data = await response.json();
-  //   res.status(200).json(data);
-  // } else {
-  //   // Not Signed in
-  //   res.status(401);
-  // }
+  const stringifiedToken = await getToken({ req, secret });
+  const session = await getSession({ req });
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
+  if (session) {
+    // Signed in
+    const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
+    const externalId = stringifiedToken.sub;
+    const response = await fetch(
+      `${baseUrlStore}/v1/customers/54${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
 }
