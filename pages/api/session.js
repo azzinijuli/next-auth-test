3f58df254f69e3cc267ddc6340bcf1aa1891431a
Juli Azzini <jazzini@teco.com.ar>
2021-11-08 12:07:54 -0300

    fix fetch

--- pages/api/session.js
+++ pages/api/session.js
@@ -2,14 +2,20 @@ import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJzb25hbCIsImluc3RhbmNlIjoiNjEifQ.Uq-dOKkLtkZQppeygKvRazDig2hcBnebOIYphdFH62M";
   if (session) {
     // Signed in
     const baseUrlStore = "https://backoffice-staging.personal-svcs.com"; //UAT
     const externalId = session.user?.name;
     const response = await fetch(
-      `${baseUrlStore}/v1/customers/${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
+      `${baseUrlStore}/v1/customers/54${externalId}}/products?status=PURCHASED,CANCELLED,EXPIRED`,
       {
         method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
     const data = await response.json();
